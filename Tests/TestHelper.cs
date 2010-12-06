31aea3556fc52c70a407503be7e22768aca89292
Anthony Steele <Anthony@PlasticAvalon.org>
2010-12-06 15:46:57 +0000

    More tests on solved boards

--- Tests/TestHelper.cs
+++ Tests/TestHelper.cs
@@ -1,5 +1,6 @@
 ﻿namespace Tests
 {
+    using System.Collections.Generic;
     using NUnit.Framework;
 
     public static class TestHelper
@@ -10,6 +11,39 @@
             {
                 Assert.Greater(value, 0);
                 Assert.Less(value, 10);
+            }
+
+            TestValuesAreUniqueInColumn(solvedBoard);
+            TestvaluesAreUniqueInRow(solvedBoard);
+        }
+
+        private static void TestvaluesAreUniqueInRow(int[,] solvedBoard)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                List<int> rowValues = new List<int>();
+                for (int x = 0; x < 9; x++)
+                {
+                    // a cell's value has not occurend before in the row
+                    int newValue = solvedBoard[x, y];
+                    Assert.IsFalse(rowValues.Contains(newValue), "Found duplicate cell value " + newValue + " in row");
+                    rowValues.Add(newValue);
+                }
+            }
+        }
+
+        private static void TestValuesAreUniqueInColumn(int[,] solvedBoard)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                List<int> columnValues = new List<int>();
+                for (int y = 0; y < 9; y++)
+                {
+                    // a cell's value has not occurend before in the column
+                    int newValue = solvedBoard[x, y];
+                    Assert.IsFalse(columnValues.Contains(newValue), "Found duplicate cell value " + newValue + " in column");
+                    columnValues.Add(newValue);
+                }
             }
         }
     }
